6e5f06319d0d8855f01214fcad03e7ebb1fc4e1a
leonematias <leonematias@gmail.com>
2017-04-15 20:51:11 -0400

    New robot code

--- ArduinoRobot/src/arduino/RobotCode3.ino
+++ ArduinoRobot/src/arduino/RobotCode3.ino
@@ -5,52 +5,45 @@
  */
 
 
-//Commands
-const char PING = 'a';
-
-
-
-//Servo library
-#include <Servo.h>
-Servo myServo;
-const int Echo = A3;  
-const int Trig = A2;
-
 //Gyroscope library
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
 #define BNO055_SAMPLERATE_DELAY_MS (100)
-Adafruit_BNO055 bno = Adafruit_BNO055(55);
+Adafruit_BNO055 gyro = Adafruit_BNO055(55); 
 
-//LED
-const int LED = 13;
 
-//Wheels motors
-const int in1 = 9;
-const int in2 = 8;
-const int in3 = 7;
-const int in4 = 6;
-const int ENA = 11;
-const int ENB = 5;
-int currentSpeed = MID_ABS;
+/**
+ * Pins layout
+ */
+const int PIN_WHEELS_IN1 = 9;
+const int PIN_WHEELS_IN2 = 8;
+const int PIN_WHEELS_IN3 = 7;
+const int PIN_WHEELS_IN4 = 6;
+const int PIN_WHEELS_ENA = 11;
+const int PIN_WHEELS_ENB = 5;
+const int PIN_LED = 13;
+const int PIN_DIST_ECHO = A3;
+const int PIN_DIST_TRIG = A2;
+//BNO055 using analog A4 and A5
+
 
 
 //Control variables
-int wheelsMotorSpeed = 100;
-int led = false;
+int wheelsMotorSpeed = 0;
+int led = 0;
 
-
-const char COMMAMD_SEP = '\n';
-const char PARAM_SEP = '|';
-const int MAX_COMMAND_PARAMS = 3;
-
+//Command
+#define COMMAMD_SEP = '\n';
+#define PARAM_SEP = '|';
+#define MAX_COMMAND_PARAMS = 3;
 struct Command {
     String command;
     int paramsCount;
     String params[COMMAND];
 };
+Command cmd;
 
 
 //------------------------ Utils ------------------------//
@@ -58,9 +51,8 @@ struct Command {
 /**
  * Read incoming Serial command with format: Command;Param1;Param2;Param3;\n
  */
-Command readInputCommand() {
+void readInputCommand() {
     //Init empty command
-    Command cmd;
     cmd.command = "";
     cmd.paramsCount = 0;
     for(int i = 0; i < MAX_COMMAND_PARAMS; i++) {
@@ -74,12 +66,12 @@ Command readInputCommand() {
         String inputString = Serial.readStringUntil(COMMAMD_SEP);
         int len = inputString.length();
         if(len == 0)
-            return cmd;
+            return;
 
         //Parse command
         int sepIndex = str.indexOf(PARAM_SEP);
         if(sepIndex < 0)
-            return cmd;
+            return;
         cmd.command = str.substring(0, sepIndex);
 
         //Read params
@@ -93,14 +85,44 @@ Command readInputCommand() {
             n = sepIndex + 1;
         }
     }
-
-    return cmd;
 }
 
-void returnOk(Command cmd) {
+void returnOk() {
     String response = cmd.command;
     response += "_OK";
     Serial.println(response);
+}
+
+void returnInt(int result) {
+    String response = cmd.command;
+    response += '_';
+    response += result;
+    Serial.println(response);
+}
+
+void return3Double(double res1, double res2, double res3) {
+    String response = cmd.command;
+    response += '_';
+    response += res1;
+    response += '_';
+    response += res2;
+    response += '_';
+    response += res3;
+    Serial.println(response);
+}
+
+/**
+ * Ultrasonic distance measurement
+ */
+int distanceTest() {
+    digitalWrite(PIN_DIST_TRIG, LOW);   
+    delayMicroseconds(2);
+    digitalWrite(PIN_DIST_TRIG, HIGH);  
+    delayMicroseconds(20);
+    digitalWrite(PIN_DIST_TRIG, LOW);   
+    float fDistance = pulseIn(PIN_DIST_ECHO, HIGH);  
+    fDistance = fDistance / 58;       
+    return (int)fDistance;
 }
 
 
@@ -112,26 +134,36 @@ void returnOk(Command cmd) {
  */
 void setup() {
     //Init LED
-    pinMode(LED, OUTPUT);
+    pinMode(PIN_LED, OUTPUT);
 
     //Init Bluetooth
     Serial.begin(9600);
 
     //Init motors
-    pinMode(in1,OUTPUT);
-    pinMode(in2,OUTPUT);
-    pinMode(in3,OUTPUT);
-    pinMode(in4,OUTPUT);
-    pinMode(ENA,OUTPUT);
-    pinMode(ENB,OUTPUT);
+    pinMode(PIN_WHEELS_IN1, OUTPUT);
+    pinMode(PIN_WHEELS_IN2, OUTPUT);
+    pinMode(PIN_WHEELS_IN3, OUTPUT);
+    pinMode(PIN_WHEELS_IN4, OUTPUT);
+    pinMode(PIN_WHEELS_ENA, OUTPUT);
+    pinMode(PIN_WHEELS_ENB, OUTPUT);
 
-    //Init servo
-    myServo.attach(3);// attach servo on pin 3 to servo object
-    pinMode(Echo, INPUT);    
-    pinMode(Trig, OUTPUT);  
+    //Init Distance sensor
+    pinMode(PIN_DIST_ECHO, INPUT);    
+    pinMode(PIN_DIST_TRIG, OUTPUT);  
 
     //Stop wheels
-    stopMovement();
+    digitalWrite(PIN_WHEELS_ENA, LOW);
+    digitalWrite(PIN_WHEELS_ENB, LOW);
+
+    //Initialise gyroscope
+    if(!gyro.begin()) {
+      Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+      while(1);
+    }
+    delay(1000);
+    gyro.setExtCrystalUse(true);
+
+
 }
 
 /**
@@ -139,17 +171,77 @@ void setup() {
  */
 void loop() {
 
-    //Read incomming Bluetooth commands
-    Command cmd = readInputCommand();
+    //Read incomming Bluetooth commands and populate global var cmd
+    readInputCommand();
     if(cmd.command == "")
         return;
 
-    
+    //Ping command
     if(cmd.command == "PING") {
-        returnOk(cmd);
+        returnOk();
 
-    } else if(cmd.command == "SPEED") {
+    //Enable/disable led
+    } else if(cmd.command == "LED") {
+        led = cmd.params[0].toInt();
+        digitalWrite(PIN_LED, led == 1 ? HIGH : LOW);
+        returnOk();
+
+    //Set wheels speed
+    } else if(cmd.command == "SPD") {
         wheelsMotorSpeed = cmd.params[0].toInt();
-        returnOk(cmd);
+        analogWrite(PIN_WHEELS_ENA, wheelsMotorSpeed);
+        analogWrite(PIN_WHEELS_ENB, wheelsMotorSpeed);
+        returnOk();
+
+    //Set wheels in forward mode
+    } else if(cmd.command == "FORW") {
+        digitalWrite(PIN_WHEELS_IN1, LOW);
+        digitalWrite(PIN_WHEELS_IN2, HIGH);
+        digitalWrite(PIN_WHEELS_IN3, LOW);
+        digitalWrite(PIN_WHEELS_IN4, HIGH);
+        returnOk();
+
+    //Set wheels in backward mode
+    } else if(cmd.command == "BACK") {
+        digitalWrite(PIN_WHEELS_IN1, HIGH);
+        digitalWrite(PIN_WHEELS_IN2, LOW);
+        digitalWrite(PIN_WHEELS_IN3, HIGH);
+        digitalWrite(PIN_WHEELS_IN4, LOW);
+        returnOk();
+
+    //Stop wheels movement
+    } else if(cmd.command == "STOP") {
+        wheelsMotorSpeed = 0;
+        analogWrite(PIN_WHEELS_ENA, wheelsMotorSpeed);
+        analogWrite(PIN_WHEELS_ENB, wheelsMotorSpeed);
+        returnOk();
+
+    //Set wheels in rotate left mode
+    } else if(cmd.command == "RLEFT") {
+        digitalWrite(PIN_WHEELS_IN1, LOW);
+        digitalWrite(PIN_WHEELS_IN2, HIGH);
+        digitalWrite(PIN_WHEELS_IN3, HIGH);
+        digitalWrite(PIN_WHEELS_IN4, LOW);
+        returnOk();
+
+    //Set wheels in rotate right mode
+    } else if(cmd.command == "RRIGHT") {
+        digitalWrite(PIN_WHEELS_IN1, HIGH);
+        digitalWrite(PIN_WHEELS_IN2, LOW);
+        digitalWrite(PIN_WHEELS_IN3, LOW);
+        digitalWrite(PIN_WHEELS_IN4, HIGH);
+        returnOk();
+
+    //Distance test
+    } else if(cmd.command == "DIST") {
+        int distance = distanceTest();
+        returnInt(distance);
+
+    //Get gyroscope info
+    } else if(cmd.command == "GYRO") {
+        sensors_event_t event;
+        gyro.getEvent(&event);
+        return3Double(event.orientation.x, orientation.y, orientation.z);
+        //delay(BNO055_SAMPLERATE_DELAY_MS);
     }
 }
